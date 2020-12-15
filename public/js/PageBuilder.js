bf5207f70c1087ce26010c2a897fd792bfd8c227
hermancai <30858461+hermancai@users.noreply.github.com>
2020-12-15 03:44:04 -0800

    build html input form

--- public/js/PageBuilder.js
+++ public/js/PageBuilder.js
@@ -4,13 +4,13 @@ class PageBuilder {
     }
 
     loadPage() {
-        document.getElementById("clickhere").addEventListener("click", function(event) {
-            event.preventDefault();
-            var drm = new DataRequestManager();
-            var url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=5";
+        // document.getElementById("clickhere").addEventListener("click", function(event) {
+        //     event.preventDefault();
+        //     var drm = new DataRequestManager();
+        //     var url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=5";
 
-            drm.getData(url);
-        })
+        //     drm.getData(url);
+        // })
     }
 }
 
